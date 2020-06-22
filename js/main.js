3fef01f88e0aacd7a5129f958bcbbb1c141eb1a5
zare926 <ilove.boc926@gmail.com>
2020-06-22 22:57:58 +0900

    js activeクラス付与

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+  const menuItems = document.querySelectorAll('.menu li a');
+  menuItems.forEach(item => {
+    item.addEventListener('click',e => {
+      e.preventDefault();
+
+      item.classList.add('active');
+    });
+  });
+}
